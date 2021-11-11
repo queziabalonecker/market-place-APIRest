06bd220862faf68212aaeab6000a77cd371ec5f2
Quezia <queziaamenezes@gmail.com>
2021-11-11 15:51:30 -0300

    retirada de comentários

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,14 @@ const loginVerification = require('./middlewares/loginVerification');
 
 const routes = express();
 
-//usuarios
 routes.post('/usuarios', users.cadastrarUsuario);
 routes.post('/login', users.login);
 
-//rotas com autenticação necessária
 routes.use(loginVerification);
 
 routes.get('/usuarios', users.detalharUsuario);
 routes.put('/usuarios', users.atualizarUsuario);
 
-//produtos
 routes.post('/produtos', products.cadastrarProduto);
 routes.get('/produtos', products.listarProdutos);
 routes.get('/produtos/:id', products.detalharProduto);
